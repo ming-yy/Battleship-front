1b012258496663bbc4e06b3996fe434980d341c4
ming-yy <mingeraneos@gmail.com>
2024-03-15 13:44:41 +0100

    home page done
    secondary content missing

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,27 +1,30 @@
 import { Navbar } from "../Components/Navbar";
 import '../Styles/style.css';
+import '../Styles/home-style.css';
 import GameDemoImg from '../Images/home-game-demo.png';
 
 export function Home() {
     return (
         <div className="main-page-container">
             <Navbar/>
-            <div className="home-main-container">
+            <div className="home-container">
                 <div className="home-main-content-container">
-                    <img src={ GameDemoImg } />
+                    <div className="home-main-img-container">
+                        <img src={ GameDemoImg } />
+                    </div>
                     <div className="home-main-content">
                         <h1 className="home-banner-container">
                             Juega a Hundir la Flota
                         </h1>
-                        <button className="button-online-game">
+                        <button className="home-button">
                             <span> Jugar Online </span>
                         </button>
-                        <button className="button-offline-game">
+                        <button className="home-button">
                             <span> Jugar Offline </span>
                         </button>
                     </div>
                 </div>
-                <div className="home-secondary-content">
+                <div className="home-secondary-content-container">
 
                 </div>
             </div>
